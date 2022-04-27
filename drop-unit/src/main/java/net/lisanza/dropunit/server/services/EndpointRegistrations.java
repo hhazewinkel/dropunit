6c06fdf59c15be838b6b173a9db611b3c51b0a52
Harrie Hazewinkel <harrie.hazewinkel@kb.nl>
2022-04-27 09:00:20 +0200

    Separate find mechanism.

--- drop-unit/src/main/java/net/lisanza/dropunit/server/services/EndpointRegistrations.java
+++ drop-unit/src/main/java/net/lisanza/dropunit/server/services/EndpointRegistrations.java
@@ -13,18 +13,34 @@ public class EndpointRegistrations extends ArrayList<DropUnitEndpoint> {
     private static final Logger LOGGER = LoggerFactory.getLogger(EndpointRegistrations.class);
 
     public List<DropUnitEndpoint> findByUrlAndMethod(String url, String method) {
-        List<DropUnitEndpoint> subList = new ArrayList<>();
-        for (DropUnitEndpoint endpoint : this) {
-            LOGGER.debug("findByUrlAndMethod: {} <> {} | {} <> {}",
-                    url, endpoint.getUrl(),
-                    method, endpoint.getMethod());
+        List<DropUnitEndpoint> subListByMethod = findByMethod(method, this);
+        LOGGER.info("findByUrlAndMethod (by method): {}", subListByMethod.size());
+        List<DropUnitEndpoint> subList = findByUrl(url, subListByMethod);
+        LOGGER.info("findByUrlAndMethod: {}", subList.size());
+        return subList;
+    }
 
-            if (matchesUrl(url, endpoint.getUrl())
-                    && matchesMethod(method, endpoint.getMethod())) {
+    public List<DropUnitEndpoint> findByMethod(String method, List<DropUnitEndpoint> endpoints) {
+        List<DropUnitEndpoint> subList = new ArrayList<>();
+        for (DropUnitEndpoint endpoint : endpoints) {
+            LOGGER.debug("findByMethod({}) <> {} (url: {})", method, endpoint.getMethod(), endpoint.getUrl());
+            if (matchesMethod(method, endpoint.getMethod())) {
                 subList.add(endpoint);
             }
         }
-        LOGGER.info("findByUrlAndMethod: {}", subList.size());
+        LOGGER.info("findByMethod: {}", subList.size());
+        return subList;
+    }
+
+    public List<DropUnitEndpoint> findByUrl(String url, List<DropUnitEndpoint> endpoints) {
+        List<DropUnitEndpoint> subList = new ArrayList<>();
+        for (DropUnitEndpoint endpoint : endpoints) {
+            LOGGER.debug("findByUrl({}) <> {}", url, endpoint.getUrl());
+            if (matchesUrl(url, endpoint.getUrl())) {
+                subList.add(endpoint);
+            }
+        }
+        LOGGER.info("findByUrl: {}", subList.size());
         return subList;
     }
 
@@ -52,8 +68,10 @@ public class EndpointRegistrations extends ArrayList<DropUnitEndpoint> {
 
     private boolean matchesUrl(String requestUrl, String endpointUrl) {
         if (endpointUrl.endsWith("?")) {
+            LOGGER.debug("endpoint ends with ? and {} starts with {} ({})", endpointUrl, requestUrl, endpointUrl.startsWith(requestUrl));
             return requestUrl.startsWith(endpointUrl);
         }
+        LOGGER.debug("endpoint full {} equals {} ({})", endpointUrl, requestUrl, endpointUrl.equals(requestUrl));
         return requestUrl.equals(endpointUrl);
     }
 
